72f2b664162ab8f68db55915cac9c2ea4343e7bc
kl1mch1k <klimov.lenar@yandex.ru>
2022-03-22 16:17:37 +0300

    jobs

--- data/jobs.py
+++ data/jobs.py
@@ -3,7 +3,7 @@ import sqlalchemy
 from .db_session import SqlAlchemyBase
 
 
-class Jobs(SqlAlchemyBase):
+class Job(SqlAlchemyBase):
     __tablename__ = 'jobs'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+
 from flask import Flask
 from data import db_session
-from data.users import User
+from data.jobs import Job
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -9,19 +11,14 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/mars_explorer.db")
     db_sess = db_session.create_session()
-    for i in [['Scott', 'Ridley', 21, 'captain', 'research engineer', 'module_1', 'scott_chief@mars.org'],
-              ['Klimov', 'Lenar', 16, 'officer', 'engineer', 'module_2', 'klimov_lenar@mars.org'],
-              ['Agliuillin', 'Ilsaf', 15, 'lieutenant', 'researcher', 'module_3', 'ilsaf_agliullin@mars.org'],
-              ['Valeev', 'Karim', 15, 'capral', 'programmer', 'module_4', 'karim_valeev@mars.org']]:
-        user = User()
-        user.surname = i[0]
-        user.name = i[1]
-        user.age = i[2]
-        user.position = i[3]
-        user.speciality = i[4]
-        user.address = i[5]
-        user.email = i[6]
-        db_sess.add(user)
+    job = Job()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.start_date = datetime.datetime.now()
+    job.is_finished = False
+    db_sess.add(job)
     db_sess.commit()
     # app.run()
 
