8c354a671eac0dda7eb2faa54c547536998de807
kl1mch1k <klimov.lenar@yandex.ru>
2022-03-27 12:34:19 +0300

    register

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-import datetime
-
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
-from data.jobs import Jobs
+from data.db_session import global_init
 from data.users import User
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
-def works():
-    db_session.global_init('db/mars_explorer.db')
-    db_sess = db_session.create_session()
-    jobs = db_sess.query(Jobs, User).filter(Jobs.team_leader == User.id)
-    return render_template("works.html", jobs=jobs)
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 def main():
+    global_init('db/mars_explorer.db')
     app.run()
 
 
